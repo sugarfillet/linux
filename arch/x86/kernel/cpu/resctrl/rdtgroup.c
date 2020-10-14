bc542b75d3c1efb6a41c6b6c9904d5861acbf1e6
James Morse <james.morse@arm.com>
2020-10-14 12:26:26 +0100

    x86/resctrl: Allow rmid allocation to be scoped by closid
    ANBZ: #1598
    
    cherry-picked from https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git
    
    MPAMs rmid values are not unique unless the closid is considered as well.
    
    alloc_rmid() expects the rmid to be an independent number.
    
    Pass the closid in to alloc_rmid(). Use this to compare indexes when
    allocating. If the closid is not relevant to the index, this ends up
    comparing the free rmid with itself, and the first free entry will be
    used. With MPAM the closid is included in the index, so this becomes a
    walk of the free rmid entries, until one that matches the supplied
    closid is found.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Tested-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -3018,7 +3018,7 @@ static int rdtgroup_mkdir_mon(struct kernfs_node *parent_kn,
 	rdtgrp->closid = prgrp->closid;
 
 	if (rdt_mon_capable) {
-		ret = alloc_rmid();
+		ret = alloc_rmid(rdtgrp->closid);
 		if (ret < 0) {
 			rdt_last_cmd_puts("Out of RMIDs\n");
 			goto out_destroy;
@@ -3075,7 +3075,7 @@ static int rdtgroup_mkdir_ctrl_mon(struct kernfs_node *parent_kn,
 	rdtgrp->closid = ret;
 
 	if (rdt_mon_capable) {
-		ret = alloc_rmid();
+		ret = alloc_rmid(rdtgrp->closid);
 		if (ret < 0) {
 			rdt_last_cmd_puts("Out of RMIDs\n");
 			goto out_closid_free;
