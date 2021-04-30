25ef341a58bd8eaaf96a89529063b7c76342c8bd
Nicholas Piggin <npiggin@gmail.com>
2021-04-29 22:58:49 -0700

    mm/vmalloc: hugepage vmalloc mappings
    ANBZ: #23
    
    commit 121e6f3258fe393e22c36f61a319be8a4f2c05ae upstream
    
    Support huge page vmalloc mappings.  Config option HAVE_ARCH_HUGE_VMALLOC
    enables support on architectures that define HAVE_ARCH_HUGE_VMAP and
    supports PMD sized vmap mappings.
    
    vmalloc will attempt to allocate PMD-sized pages if allocating PMD size or
    larger, and fall back to small pages if that was unsuccessful.
    
    Architectures must ensure that any arch specific vmalloc allocations that
    require PAGE_SIZE mappings (e.g., module allocations vs strict module rwx)
    use the VM_NOHUGE flag to inhibit larger mappings.
    
    This can result in more internal fragmentation and memory overhead for a
    given allocation, an option nohugevmalloc is added to disable at boot.
    
    [colin.king@canonical.com: fix read of uninitialized pointer area]
      Link: https://lkml.kernel.org/r/20210318155955.18220-1-colin.king@canonical.com
    
    Link: https://lkml.kernel.org/r/20210317062402.533919-14-npiggin@gmail.com
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Ding Tianhong <dingtianhong@huawei.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [Bikuan Zhang: include header files]
    [Bikuan Zhang: modify variable definitions in __vmalloc_area_node]
    Signed-off-by: Bikuan Zhang <bikuan@linux.alibaba.com>
    Acked-by: Xu Yu <xuyu@linux.alibaba.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -72,6 +72,8 @@
 #include <linux/khugepaged.h>
 #include <linux/fault_event.h>
 #include <linux/kfence.h>
+#include <linux/buffer_head.h>
+#include <linux/vmalloc.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -8275,6 +8277,7 @@ void *__init alloc_large_system_hash(const char *tablename,
 	void *table = NULL;
 	gfp_t gfp_flags;
 	bool virt;
+	bool huge;
 
 	/* allow the kernel cmdline to have a say */
 	if (!numentries) {
@@ -8342,6 +8345,7 @@ void *__init alloc_large_system_hash(const char *tablename,
 		} else if (get_order(size) >= MAX_ORDER || hashdist) {
 			table = __vmalloc(size, gfp_flags);
 			virt = true;
+			huge = is_vm_area_hugepages(table);
 		} else {
 			/*
 			 * If bucketsize is not a power-of-two, we may free
@@ -8358,7 +8362,7 @@ void *__init alloc_large_system_hash(const char *tablename,
 
 	pr_info("%s hash table entries: %ld (order: %d, %lu bytes, %s)\n",
 		tablename, 1UL << log2qty, ilog2(size) - PAGE_SHIFT, size,
-		virt ? "vmalloc" : "linear");
+		virt ? (huge ? "vmalloc hugepage" : "vmalloc") : "linear");
 
 	if (_hash_shift)
 		*_hash_shift = log2qty;
