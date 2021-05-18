7fa808b52f2942b3081b1108f259344bd14683b4
Max Gurtovoy <mgurtovoy@nvidia.com>
2021-05-18 22:21:32 +0300

    vfio: centralize module refcount in subsystem layer
    ANBZ: #1087
    
    commit 9dcf01d95721261844d8c07c142efc143f7d38e3 upstream.
    
    Remove code duplication and move module refcounting to the subsystem
    module.
    
    Intel-SIG: commit 9dcf01d95721 vfio: centralize module refcount in subsystem layer.
    SIOV VDCM feature backporting.
    
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Link: https://lore.kernel.org/r/20210518192133.59195-2-mgurtovoy@nvidia.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    [ Fengqian Gao: amend commit log ]
    Signed-off-by: Fengqian Gao <fengqian.gao@intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -577,8 +577,6 @@ static void vfio_pci_release(struct vfio_device *core_vdev)
 	}
 
 	mutex_unlock(&vdev->reflck->lock);
-
-	module_put(THIS_MODULE);
 }
 
 static int vfio_pci_open(struct vfio_device *core_vdev)
@@ -586,9 +584,6 @@ static int vfio_pci_open(struct vfio_device *core_vdev)
 	struct vfio_pci_device *vdev =
 		container_of(core_vdev, struct vfio_pci_device, vdev);
 	int ret = 0;
-
-	if (!try_module_get(THIS_MODULE))
-		return -ENODEV;
 
 	mutex_lock(&vdev->reflck->lock);
 
@@ -603,8 +598,6 @@ static int vfio_pci_open(struct vfio_device *core_vdev)
 	vdev->refcnt++;
 error:
 	mutex_unlock(&vdev->reflck->lock);
-	if (ret)
-		module_put(THIS_MODULE);
 	return ret;
 }
 
