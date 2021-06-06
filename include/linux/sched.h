78e9e41b5040f6b345cc2401f5d6aa83d2e41858
Xunlei Pang <xlpang@linux.alibaba.com>
2021-06-06 17:05:24 +0800

    ck: proc/loadavg: Add load support for rich containr
    to #34559224
    
    Fetch nr_running and nr_uninterruptible from scheduler(cpu cgroup)
    and use them to calculate per-cgroup load like system global load.
    
    Note that calc_cgroup_load() walks the whole hierarchy and calculate
    load periodically, it can cause overhead if too many cpu cgroups.
    
    Make /proc/loadavg container aware.
    
    "uptime" command will show the per-cgroup load of the rich container.
    
    Reviewed-by: Michael Wang <yun.wang@linux.alibaba.com>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>
    Reviewed-by: Yihao Wu <wuyihao@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2151,6 +2151,8 @@ bool child_cpuacct(struct task_struct *tsk);
 void cpuacct_get_usage_result(struct task_struct *tsk, int cpu,
 		struct cpuacct_usage_result *res);
 unsigned long task_ca_running(struct task_struct *tsk, int cpu);
+void get_cgroup_avenrun(struct task_struct *tsk, unsigned long *loads,
+		unsigned long offset, int shift);
 bool check_rich_container(unsigned int cpu, unsigned int *index,
 		bool *rich_container, unsigned int *total);
 
@@ -2162,6 +2164,9 @@ static inline unsigned long task_ca_running(struct task_struct *tsk, int cpu)
 {
 	return 0;
 }
+
+static inline void get_cgroup_avenrun(struct task_struct *tsk,
+		unsigned long *loads, unsigned long offset, int shift) { }
 
 static inline bool check_rich_container(unsigned int cpu, unsigned int *index,
 		bool *rich_container, unsigned int *total)
