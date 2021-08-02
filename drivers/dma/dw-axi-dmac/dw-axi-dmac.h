cf44a1a1c7723dbc577453b54c8406f89ed428da
Pandith N <pandith.n@intel.com>
2021-08-02 11:24:52 +0530

    dmaengine: dw-axi-dmac: Remove free slot check algorithm in dw_axi_dma_set_hw_channel
    commit 32286e2793858a8491c2276b2754e9850467bb6b upstream
    
    Removed free slot check algorithm in dw_axi_dma_set_hw_channel. For 8
    DMA channels, use respective handshake slot in DMA_HS_SEL APB register.
    
    For every channel, an dedicated slot is provided in  hardware handshake
    register AXIDMA_CTRL_DMA_HS_SEL_n. Peripheral source number is
    programmed in respective channel slots.
    
    Signed-off-by: Pandith N <pandith.n@intel.com>
    Tested-by: Pan Kris <kris.pan@intel.com>
    Link: https://lore.kernel.org/r/20210802055454.15192-2-pandith.n@intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- drivers/dma/dw-axi-dmac/dw-axi-dmac.h
+++ drivers/dma/dw-axi-dmac/dw-axi-dmac.h
@@ -186,6 +186,9 @@ static inline struct axi_dma_chan *dchan_to_axi_dma_chan(struct dma_chan *dchan)
 #define UNUSED_CHANNEL		0x3F /* Set unused DMA channel to 0x3F */
 #define MAX_BLOCK_SIZE		0x1000 /* 1024 blocks * 4 bytes data width */
 
+#define DMA_APB_HS_SEL_BIT_SIZE		0x08 /* HW handshake bits per channel */
+#define DMA_APB_HS_SEL_MASK	0xFF /* HW handshake select masks */
+
 /* DMAC_CFG */
 #define DMAC_EN_POS			0
 #define DMAC_EN_MASK			BIT(DMAC_EN_POS)
