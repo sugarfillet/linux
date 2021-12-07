5ad91cf03ecbbca1f5924b901211df87e5436c19
You Xiao <youxiao.fc@linux.alibaba.com>
2021-12-07 13:29:43 +0800

    phy: synopsys: dw-mipi-dphy: add PHY TMR configures
    Add DPHY data & clock lane timers configuration according
    to Synopsys PHY specification to give correct configs for
    each different bit clock.
    
    Signed-off-by: You Xiao <youxiao.fc@linux.alibaba.com>

--- drivers/phy/synopsys/phy-dw-mipi-dphy.c
+++ drivers/phy/synopsys/phy-dw-mipi-dphy.c
@@ -62,6 +62,8 @@
     #define PHY_TST_CTRL1_TESTIN			GENMASK( 7,  0)
 #define DW_PHY_CAL					0xcc
     #define PHY_CAL_TXSKEWCALHS				BIT(0)
+#define DW_PHY_TMR_RD_CFG				0xf4
+    #define PHY_TMR_RD_CFG_MAX_RD_TIME			GENMASK(14,  0)
 
 /* -------------------------- DPHY Test Code ------------------------ */
 
@@ -137,6 +139,10 @@ struct dw_pll_range {
 	u32 data_rate;
 	u8  hsfreqrange;
 	u8  vco_range;
+	u16 clk_lp2hs;
+	u16 clk_hs2lp;
+	u16 data_lp2hs;
+	u16 data_hs2lp;
 };
 
 struct dw_dphy_cfg {
@@ -146,6 +152,10 @@ struct dw_dphy_cfg {
 	u8 vco_range;
 	u8 n_div;
 	u8 m_div;
+	u16 clk_lp2hs;
+	u16 clk_hs2lp;
+	u16 data_lp2hs;
+	u16 data_hs2lp;
 };
 
 struct dw_dphy {
@@ -159,72 +169,80 @@ struct dw_dphy {
 	struct clk *pclk;		/* APB slave bus clock  */
 };
 
-#define dw_fill_range(r, f, v) { .data_rate = r, .hsfreqrange = f, .vco_range = v }
+#define dw_fill_range(r, f, v, cl, ch, dl, dh) {	\
+	.data_rate	= r,				\
+	.hsfreqrange	= f,				\
+	.vco_range	= v,				\
+	.clk_lp2hs	= cl,				\
+	.clk_hs2lp	= ch,				\
+	.data_lp2hs	= dl,				\
+	.data_hs2lp	= dh				\
+}
 
 static const struct dw_pll_range pll_range_table[] = {
-	dw_fill_range(80, 0x00, 0x3F),
-	dw_fill_range(90, 0x10, 0x3F),
-	dw_fill_range(100, 0x20, 0x3F),
-	dw_fill_range(110, 0x30, 0x39),
-	dw_fill_range(120, 0x01, 0x39),
-	dw_fill_range(130, 0x11, 0x39),
-	dw_fill_range(140, 0x21, 0x39),
-	dw_fill_range(150, 0x31, 0x39),
-	dw_fill_range(160, 0x02, 0x39),
-	dw_fill_range(170, 0x12, 0x2F),
-	dw_fill_range(180, 0x22, 0x2F),
-	dw_fill_range(190, 0x32, 0x2F),
-	dw_fill_range(205, 0x03, 0x2F),
-	dw_fill_range(220, 0x13, 0x29),
-	dw_fill_range(235, 0x23, 0x29),
-	dw_fill_range(250, 0x33, 0x29),
-	dw_fill_range(275, 0x04, 0x29),
-	dw_fill_range(300, 0x14, 0x29),
-	dw_fill_range(325, 0x25, 0x29),
-	dw_fill_range(350, 0x35, 0x1F),
-	dw_fill_range(400, 0x05, 0x1F),
-	dw_fill_range(450, 0x16, 0x19),
-	dw_fill_range(500, 0x26, 0x19),
-	dw_fill_range(550, 0x37, 0x19),
-	dw_fill_range(600, 0x07, 0x19),
-	dw_fill_range(650, 0x18, 0x19),
-	dw_fill_range(700, 0x28, 0x0F),
-	dw_fill_range(750, 0x39, 0x0F),
-	dw_fill_range(800, 0x09, 0x0F),
-	dw_fill_range(850, 0x19, 0x0F),
-	dw_fill_range(900, 0x29, 0x09),
-	dw_fill_range(950, 0x3A, 0x09),
-	dw_fill_range(1000, 0x0A, 0x09),
-	dw_fill_range(1050, 0x1A, 0x09),
-	dw_fill_range(1100, 0x2A, 0x09),
-	dw_fill_range(1150, 0x3B, 0x09),
-	dw_fill_range(1200, 0x0B, 0x09),
-	dw_fill_range(1250, 0x1B, 0x09),
-	dw_fill_range(1300, 0x2B, 0x09),
-	dw_fill_range(1350, 0x3C, 0x03),
-	dw_fill_range(1400, 0x0C, 0x03),
-	dw_fill_range(1450, 0x1C, 0x03),
-	dw_fill_range(1500, 0x2C, 0x03),
-	dw_fill_range(1550, 0x3D, 0x03),
-	dw_fill_range(1600, 0x0D, 0x03),
-	dw_fill_range(1650, 0x1D, 0x03),
-	dw_fill_range(1700, 0x2E, 0x03),
-	dw_fill_range(1750, 0x3E, 0x03),
-	dw_fill_range(1800, 0x0E, 0x03),
-	dw_fill_range(1850, 0x1E, 0x03),
-	dw_fill_range(1900, 0x2F, 0x03),
-	dw_fill_range(1950, 0x3F, 0x03),
-	dw_fill_range(2000, 0x0F, 0x03),
-	dw_fill_range(2050, 0x40, 0x03),
-	dw_fill_range(2100, 0x41, 0x03),
-	dw_fill_range(2150, 0x42, 0x03),
-	dw_fill_range(2200, 0x43, 0x03),
-	dw_fill_range(2250, 0x44, 0x03),
-	dw_fill_range(2300, 0x45, 0x01),
-	dw_fill_range(2350, 0x46, 0x01),
-	dw_fill_range(2400, 0x47, 0x01),
-	dw_fill_range(2450, 0x48, 0x01),
-	dw_fill_range(2500, 0x49, 0x01),
+	dw_fill_range(80,  0x00, 0x3F, 21, 17, 15, 10),
+	dw_fill_range(90,  0x10, 0x3F, 23, 17, 16, 10),
+	dw_fill_range(100, 0x20, 0x3F, 22, 17, 16, 10),
+	dw_fill_range(110, 0x30, 0x39, 25, 18, 17, 11),
+	dw_fill_range(120, 0x01, 0x39, 26, 20, 18, 11),
+	dw_fill_range(130, 0x11, 0x39, 27, 19, 19, 11),
+	dw_fill_range(140, 0x21, 0x39, 27, 19, 19, 11),
+	dw_fill_range(150, 0x31, 0x39, 28, 20, 20, 12),
+	dw_fill_range(160, 0x02, 0x39, 30, 21, 22, 13),
+	dw_fill_range(170, 0x12, 0x2F, 30, 21, 23, 13),
+	dw_fill_range(180, 0x22, 0x2F, 31, 21, 23, 13),
+	dw_fill_range(190, 0x32, 0x2F, 32, 22, 24, 13),
+	dw_fill_range(205, 0x03, 0x2F, 35, 22, 25, 13),
+	dw_fill_range(220, 0x13, 0x29, 37, 26, 27, 15),
+	dw_fill_range(235, 0x23, 0x29, 38, 28, 27, 16),
+	dw_fill_range(250, 0x33, 0x29, 41, 29, 30, 17),
+	dw_fill_range(275, 0x04, 0x29, 43, 29, 32, 18),
+	dw_fill_range(300, 0x14, 0x29, 45, 32, 35, 19),
+	dw_fill_range(325, 0x25, 0x29, 48, 33, 36, 18),
+	dw_fill_range(350, 0x35, 0x1F, 51, 35, 40, 20),
+	dw_fill_range(400, 0x05, 0x1F, 59, 37, 44, 21),
+	dw_fill_range(450, 0x16, 0x19, 65, 40, 49, 23),
+	dw_fill_range(500, 0x26, 0x19, 71, 41, 54, 24),
+	dw_fill_range(550, 0x37, 0x19, 77, 44, 57, 26),
+	dw_fill_range(600, 0x07, 0x19, 82, 46, 64, 27),
+	dw_fill_range(650, 0x18, 0x19, 87, 48, 67, 28),
+	dw_fill_range(700, 0x28, 0x0F, 94, 52, 71, 29),
+	dw_fill_range(750, 0x39, 0x0F, 99, 52, 75, 31),
+	dw_fill_range(800, 0x09, 0x0F, 105, 55, 82, 32),
+	dw_fill_range(850, 0x19, 0x0F, 110, 58, 85, 32),
+	dw_fill_range(900, 0x29, 0x09, 115, 58, 88, 35),
+	dw_fill_range(950, 0x3A, 0x09, 120, 62, 93, 36),
+	dw_fill_range(1000, 0x0A, 0x09, 128, 63, 99, 38),
+	dw_fill_range(1050, 0x1A, 0x09, 132, 65, 102, 38),
+	dw_fill_range(1100, 0x2A, 0x09, 138, 67, 106, 39),
+	dw_fill_range(1150, 0x3B, 0x09, 146, 69, 112, 42),
+	dw_fill_range(1200, 0x0B, 0x09, 151, 71, 117, 43),
+	dw_fill_range(1250, 0x1B, 0x09, 153, 74, 120, 45),
+	dw_fill_range(1300, 0x2B, 0x09, 160, 73, 124, 46),
+	dw_fill_range(1350, 0x3C, 0x03, 165, 76, 130, 47),
+	dw_fill_range(1400, 0x0C, 0x03, 172, 78, 134, 49),
+	dw_fill_range(1450, 0x1C, 0x03, 177, 80, 138, 49),
+	dw_fill_range(1500, 0x2C, 0x03, 183, 81, 143, 52),
+	dw_fill_range(1550, 0x3D, 0x03, 191, 84, 147, 52),
+	dw_fill_range(1600, 0x0D, 0x03, 194, 85, 152, 52),
+	dw_fill_range(1650, 0x1D, 0x03, 201, 86, 155, 53),
+	dw_fill_range(1700, 0x2E, 0x03, 208, 88, 161, 53),
+	dw_fill_range(1750, 0x3E, 0x03, 212, 89, 165, 53),
+	dw_fill_range(1800, 0x0E, 0x03, 220, 90, 171, 54),
+	dw_fill_range(1850, 0x1E, 0x03, 223, 92, 175, 54),
+	dw_fill_range(1900, 0x2F, 0x03, 231, 91, 180, 55),
+	dw_fill_range(1950, 0x3F, 0x03, 236, 95, 185, 56),
+	dw_fill_range(2000, 0x0F, 0x03, 243, 97, 190, 56),
+	dw_fill_range(2050, 0x40, 0x03, 248, 99, 194, 58),
+	dw_fill_range(2100, 0x41, 0x03, 252, 100, 199, 59),
+	dw_fill_range(2150, 0x42, 0x03, 259, 102, 204, 61),
+	dw_fill_range(2200, 0x43, 0x03, 266, 105, 210, 62),
+	dw_fill_range(2250, 0x44, 0x03, 269, 109, 213, 63),
+	dw_fill_range(2300, 0x45, 0x01, 272, 109, 217, 65),
+	dw_fill_range(2350, 0x46, 0x01, 281, 112, 225, 66),
+	dw_fill_range(2400, 0x47, 0x01, 283, 115, 226, 66),
+	dw_fill_range(2450, 0x48, 0x01, 282, 115, 226, 67),
+	dw_fill_range(2500, 0x49, 0x01, 281, 118, 227, 67),
 };
 
 static int dw_dphy_config_rstz(struct dw_dphy *dphy, u8 val)
@@ -499,6 +517,11 @@ static int dw_dphy_get_pll_cfg(struct dw_dphy *dphy,
 	cfg->n_div	 = best_n;
 	cfg->m_div	 = best_m;
 
+	cfg->clk_lp2hs	 = range->clk_lp2hs;
+	cfg->clk_hs2lp	 = range->clk_hs2lp;
+	cfg->data_lp2hs	 = range->data_lp2hs;
+	cfg->data_hs2lp	 = range->data_hs2lp;
+
 	return 0;
 }
 
@@ -536,6 +559,7 @@ static int dw_dphy_configure(struct phy *phy, union phy_configure_opts *opts)
 	int ret;
 	u8 data[8];
 	u32 phy_if_cfg, hsfreqrange, cfgclkfreqrange;
+	u32 phy_tmr_lpclk_cfg, phy_tmr_cfg;
 	unsigned long cfgclk_freq;
 	struct dw_dphy_cfg cfg = { 0 };
 	struct dw_dphy *dphy = phy_get_drvdata(phy);
@@ -604,6 +628,17 @@ static int dw_dphy_configure(struct phy *phy, union phy_configure_opts *opts)
 	phy_if_cfg = FIELD_PREP(PHY_IF_CFG_N_LANES, dphy_opts->lanes - 1) |
 		     FIELD_PREP(PHY_IF_CFG_PHY_STOP_WAIT_TIME, 0x2);
 	regmap_write(dphy->regmap, DW_PHY_IF_CFG, phy_if_cfg);
+
+	/* config clock & data lane timers */
+	phy_tmr_lpclk_cfg = FIELD_PREP(PHY_TMR_LPCLK_CFG_PHY_CLKHS2LP_TIME, cfg.clk_hs2lp)	|
+			    FIELD_PREP(PHY_TMR_LPCLK_CFG_PHY_CLKLP2HS_TIME, cfg.clk_lp2hs);
+	regmap_write(dphy->regmap, DW_PHY_TMR_LPCLK_CFG, phy_tmr_lpclk_cfg);
+
+	phy_tmr_cfg = FIELD_PREP(PHY_TMR_CFG_PHY_HS2LP_TIME, cfg.data_hs2lp)	|
+		      FIELD_PREP(PHY_TMR_CFG_PHY_LP2HS_TIME, cfg.data_lp2hs);
+	regmap_write(dphy->regmap, DW_PHY_TMR_CFG, phy_tmr_cfg);
+
+	regmap_write(dphy->regmap, DW_PHY_TMR_RD_CFG, 10000);
 
 	return 0;
 }
