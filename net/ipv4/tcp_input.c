63fde9b91863733cee86f535129bc285237d8caf
Kangjie Xu <kangjie.xu@linux.alibaba.com>
2022-06-17 14:48:36 +0800

    anolis: net: make tcp_drop noinline
    ANBZ: #1475
    
    tcp_drop() is inlined automatically by the compiler in previous versions,
    such that some tools(e.g. tcpdrop.bt in bpftrace) cannot funciton normally.
    The noinline attribute is added to tcp_drop() to fix this issue.
    
    Signed-off-by: Kangjie Xu <kangjie.xu@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4636,7 +4636,7 @@ static bool tcp_ooo_try_coalesce(struct sock *sk,
 	return res;
 }
 
-static void tcp_drop(struct sock *sk, struct sk_buff *skb)
+static noinline void tcp_drop(struct sock *sk, struct sk_buff *skb)
 {
 	sk_drops_add(sk, skb);
 	__kfree_skb(skb);
