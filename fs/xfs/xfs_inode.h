sugarfillet/linux

commit fc94e783ee3543dbee933b4dae9be3a3b8733d69

anolis: xfs: add ioctl to set DIO_ATOMIC_WRITE flag
ANBZ: #754

Add a new ioctl XFS_IOC_ATOMIC_WRITE_SET to allow user to set atomic
write. This will only be set at the first time (empty file), and later
just check if the file supports atomic write.

Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>

fs/xfs/xfs_inode.h
@@ -467,6 +467,8 @@ extern struct kmem_zone	*xfs_inode_zone;
 
 /* The default CoW extent size hint. */
 #define XFS_DEFAULT_COWEXTSZ_HINT 32
+/* atomic write file extent size (16k) */
+#define XFS_ATOMIC_WRITE_EXTSZ_HINT 4
 
 int xfs_iunlink_init(struct xfs_perag *pag);
 void xfs_iunlink_destroy(struct xfs_perag *pag);

fs/xfs/xfs_ioctl.c
@@ -2355,6 +2355,35 @@ xfs_file_ioctl(
 		return error;
 	}
 
+	case XFS_IOC_ATOMIC_WRITE_SET: {
+		uint32_t in;
+		static const int enable = 1;
+
+		if (!S_ISREG(inode->i_mode))
+			return -EINVAL;
+		if (!xfs_sb_version_hasreflink(&mp->m_sb))
+			return -EOPNOTSUPP;
+
+		if (get_user(in, (uint32_t __user *)arg))
+			return -EFAULT;
+		if (in != enable)
+			return -EINVAL;
+
+		if (!i_size_read(inode) || !inode->i_blocks) {
+			/* set atomic write only after file is newly created */
+			ip->i_d.di_flags2 |= XFS_DIFLAG2_DIO_ATOMIC_WRITE;
+			ip->i_d.di_flags |= XFS_DIFLAG_EXTSIZE;
+			ip->i_d.di_extsize = XFS_ATOMIC_WRITE_EXTSZ_HINT;
+
+			return 0;
+		}
+
+		/* just return if previously already set */
+		if (ip->i_d.di_flags2 & XFS_DIFLAG2_DIO_ATOMIC_WRITE)
+			return 0;
+
+		return -EOPNOTSUPP;
+	}
 	default:
 		return -ENOTTY;
 	}

fs/xfs/libxfs/xfs_fs.h
@@ -836,6 +836,7 @@ struct xfs_scrub_metadata {
 #define XFS_IOC_FSGEOMETRY	     _IOR ('X', 126, struct xfs_fsop_geom)
 #define XFS_IOC_BULKSTAT	     _IOR ('X', 127, struct xfs_bulkstat_req)
 #define XFS_IOC_INUMBERS	     _IOR ('X', 128, struct xfs_inumbers_req)
+#define XFS_IOC_ATOMIC_WRITE_SET     _IOW('X', 129, uint32_t)
 /*	XFS_IOC_GETFSUUID ---------- deprecated 140	 */
 
 

commit 46bbfefb067fb5b86ff45c76263a0e1dcdf906a2

anolis: xfs: make atomic write extent reflects block size
ANBZ: #1514

Currently we use atomic write extent hint 4 to indicate 16k, which
requires default 4k block size. This is not always true.
So reflects actual block size in superblock, and later we can also let
it configurable (e.g. during mount) to support other atomic write size,
e.g. 8k for postgresql.

Fixes: fc94e783ee35 ("anolis: xfs: add ioctl to set DIO_ATOMIC_WRITE flag")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>

fs/xfs/xfs_inode.h
@@ -468,7 +468,7 @@ extern struct kmem_zone	*xfs_inode_zone;
 /* The default CoW extent size hint. */
 #define XFS_DEFAULT_COWEXTSZ_HINT 32
 /* atomic write file extent size (16k) */
-#define XFS_ATOMIC_WRITE_EXTSZ_HINT 4
+#define XFS_ATOMIC_WRITE_EXTSZ_BYTES 16384
 
 int xfs_iunlink_init(struct xfs_perag *pag);
 void xfs_iunlink_destroy(struct xfs_perag *pag);

fs/xfs/xfs_ioctl.c
@@ -2083,7 +2083,7 @@ xfs_ioc_set_atomic_write(
 
 	ip->i_d.di_flags2 |= XFS_DIFLAG2_DIO_ATOMIC_WRITE;
 	ip->i_d.di_flags |= XFS_DIFLAG_EXTSIZE;
-	ip->i_d.di_extsize = XFS_ATOMIC_WRITE_EXTSZ_HINT;
+	ip->i_d.di_extsize = XFS_B_TO_FSBT(ip->i_mount, XFS_ATOMIC_WRITE_EXTSZ_BYTES);
 
 	error = xfs_trans_commit(tp);
 out:

fs/xfs/xfs_mount.c
@@ -1079,11 +1079,13 @@ xfs_atomic_staging_cancel_all(
 			while (as->aglen) {
 				xfs_fsblock_t fsbno =
 					XFS_AGB_TO_FSB(mp, agno, as->agbno);
+				xfs_extlen_t blockcount = XFS_B_TO_FSBT(mp,
+						XFS_ATOMIC_WRITE_EXTSZ_BYTES);
 
 				xfs_refcount_free_cow_extent(tp, fsbno,
-						XFS_ATOMIC_WRITE_EXTSZ_HINT);
-				as->agbno += XFS_ATOMIC_WRITE_EXTSZ_HINT;
-				as->aglen -= XFS_ATOMIC_WRITE_EXTSZ_HINT;
+						blockcount);
+				as->agbno += blockcount;
+				as->aglen -= blockcount;
 			}
 			kfree(as);
 		}